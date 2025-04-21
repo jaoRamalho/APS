331b9f24363c43a57dd59de5c29c48ee679a7e0e
Ariel Carvalho <arielwcarvalho@gmail.com>
2025-04-20 22:09:51 -0300

    criado primeiras models (elementos do banco de dados) e adicionado na tela do admin

--- controle_interno/admin.py
+++ controle_interno/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from controle_interno.models import PontoDeAcesso, Cliente, Conta
 
-# Register your models here.
+class PontoDeAcessoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'codigo', 'cliente']
+ 
+
+class ClieteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'telefone']
+
+class ContaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cliente', 'data_vencimento']
+
+
+admin.site.register(PontoDeAcesso, PontoDeAcessoAdmin)
+admin.site.register(Cliente, ClieteAdmin)
+admin.site.register(Conta, ContaAdmin)
--- controle_interno/models.py
+++ controle_interno/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class PontoDeAcesso(models.Model):
+
+    PLANOS = [
+    ("10", "10 megas"),
+    ("10F", "10 Megas Fibre"),
+    ("100", "100 Megas"),
+    ("100F", "100 Megas Fibra"),
+    ("500", "500 Megas"),
+    ("500F", "500 Megas Fibra"),
+    ]
+
+    cliente = models.ForeignKey("Cliente", on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=20)
+    data_instalacao = models.DateTimeField()
+    data_cancelamento = models.DateTimeField(null=True)
+    endereco = models.TextField()
+    plano = models.CharField(max_length=4, choices=PLANOS, default=10)
+
+class Cliente(models.Model):
+    
+    nome = models.CharField(max_length=100)
+    telefone = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Conta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    data_vencimento = models.DateTimeField()
+    
+
